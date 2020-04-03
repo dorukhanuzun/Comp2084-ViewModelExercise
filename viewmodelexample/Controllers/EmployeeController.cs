f985fe400dfdbfeca9502151d5b18ead7b15c50e
Dorukhan Uzun <200427534@student.georgianc.on.ca>
2020-04-02 22:36:05 -0400

    View Model Exercise

---
+++ viewmodelexample/Controllers/EmployeeController.cs
@@ -0,0 +1,44 @@
+﻿using viewmodelexample.ViewModels;
+using viewmodelexample.Models;
+using System.Web.Mvc;
+
+namespace viewmodelexample.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET: Employee
+        public ViewResult Details()
+        {
+            //Employee Basic Details
+            Employee employee = new Employee()
+            {
+                EmployeeId = 101,
+                Name = "Dillip",
+                Gender = "Male",
+                Department = "IT",
+                Salary = 10000,
+                AddressId = 1001
+            };
+            //Employee Address
+            Address address = new Address()
+            {
+                AddressId = 1001,
+                City = "Bhubaneswar",
+                State = "Odisha",
+                Country = "India",
+                Pin = "755019"
+            };
+            //Creating the View model
+            EmployeeDetailsViewModel employeeDetailsViewModel = new
+           EmployeeDetailsViewModel()
+            {
+                Employee = employee,
+                Address = address,
+                PageTitle = "Employee Details Page",
+                PageHeader = "Employee Details",
+            };
+            //Pass the employeeDetailsViewModel to the view
+            return View(employeeDetailsViewModel);
+        }
+    }
+}
---
+++ viewmodelexample/ViewModels/EmployeeDetailsViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using viewmodelexample.Models;
+
+namespace viewmodelexample.ViewModels
+{
+    public class EmployeeDetailsViewModel
+    {
+        public Employee Employee { get; set; }
+        public Address Address { get; set; }
+        public string PageTitle { get; set; }
+        public string PageHeader { get; set; }
+    }
+}
